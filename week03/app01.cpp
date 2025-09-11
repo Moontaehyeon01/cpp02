a237640cd07a5bbba6bc50fd4d238a2f080b7514
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-09-11 11:13:29 +0900

    week03 v.01.2

--- week03/app01.cpp
+++ week03/app01.cpp
@@ -1,17 +1,14 @@
-/********************************************************************
- * 다형성의 2가지 재료를 활용해 불완전한 다형성을 살펴보는 프로그램 *
- ********************************************************************/
 #include <iostream>
 #include <string>
 using namespace std;
 
-// 베이스 클래스의 정의
+
 class Pokemon
 {
 public:
 	void attack() const { cout << "Attack!" << endl; }
 };
-// 파생 클래스의 정의
+
 class Pikachu : public Pokemon // is-a
 {
 public:
@@ -19,13 +16,12 @@ public:
 };
 int main()
 {
-	// 베이스 클래스에 대한 포인터(소켓) 생성
+	
 	Pokemon* pokemon;
-	// ptr 포인터로 베이스 클래스의 객체 가리키기
 	pokemon = new Pokemon();
 	pokemon->attack();
 	delete pokemon;
-	// ptr 포인터로 파생 클래스의 객체 가리키기
+	
 	pokemon = new Pikachu();
 	pokemon->attack();
 	delete pokemon;
