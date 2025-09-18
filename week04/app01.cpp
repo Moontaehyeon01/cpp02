6735ae117e9e605810d8eb4f8c091bfbf8a5f06d
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-09-18 12:00:48 +0900

    week04 v0.3.1 review

--- week04/app01.cpp
+++ week04/app01.cpp
@@ -1,16 +1,7 @@
 #include "pikachu.h"
 
-//class Pikachu : public Pokemon // is-a
-//{
-//public:
-//	Pikachu() { cout << "Default(Pikachu) constructor\n"; }
-//	~Pikachu() { cout << "Derived class (Pikachu) destructor\n"; }
-//	void attack() const { cout << "Electric Attack!!" << endl; }
-//};
-
 int main()
 {
-
 	Pokemon* pokemon;
 	pokemon = new Pokemon();
 	pokemon->attack();
