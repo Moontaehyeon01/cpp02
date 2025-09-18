1606fe95494dff208238eb3f7956d5559abc3b80
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-09-18 12:26:29 +0900

    week04 v0.4 pointer

---
+++ week04/squirtle.cpp
@@ -0,0 +1,15 @@
+#include "Squirtle.h"
+
+Squirtle::Squirtle()
+{
+	cout << "Default(Squirtle) constructor\n";
+}
+
+Squirtle::~Squirtle()
+{
+	cout << "Derived class (Squirtle) destructor\n";
+}
+
+void Squirtle::attack() const {
+	cout << "Water Attack~" << endl;
+}
