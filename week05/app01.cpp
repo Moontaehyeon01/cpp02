9bdb373de4e638eb43eedae0053783f7002e72f0
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-09-25 11:36:47 +0900

    week05 v0.9 casting

--- week05/app01.cpp
+++ week05/app01.cpp
@@ -21,12 +21,17 @@ int main()
 	Animal* pa = new Dog(); // upcasting, oldstyle (C style)
 	pa->makeSound();
 
+	Cat* pc = (Cat*)pa; // Dangerous behavior
+	pc->makeSound();
+	delete pc;
+	pc = nullptr;
+
 	// Both results are same.
 	//Dog* pd = (Dog*)pa; // downcasting, oldstyle (C style)
-	Dog* pd = dynamic_cast<Dog*>(pa);//downcasting, modern style (C++11)
-	pd->makeSound();
-	delete pd;
-	pd = nullptr;
+	//Dog* pd = dynamic_cast<Dog*>(pa);//downcasting, modern style (C++11)
+	//pd->makeSound();
+	//delete pd;
+	//pd = nullptr;
 
 	return 0;
 }
