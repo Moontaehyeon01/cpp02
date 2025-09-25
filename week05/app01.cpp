3bccf9d77145ae6a0f8cf1e6b66e3b29b6f51c31
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-09-25 11:38:16 +0900

    week05 v0.9 unsafe casting

--- week05/app01.cpp
+++ week05/app01.cpp
@@ -5,33 +5,35 @@ using namespace std;
 
 class Animal {
 public:
-	virtual void makeSound() { cout << "µ¿¹°ÀÌ ¼Ò¸®¸¦ ³À´Ï´Ù~\n"; }
+	virtual void makeSound() { cout << "µ¿¹°ÀÌ ¼Ò¸®¸¦ ³À´Ï´Ù...\n"; }
 };
 class Dog : public Animal {
 public:
 	void makeSound() { cout << "¸Û¸Û!\n"; }
 };
-class Cat :public Animal {
+class Cat : public Animal {
 public:
 	void makeSound() { cout << "³Ä¿Ë~\n"; }
 };
 
 int main()
 {
-	Animal* pa = new Dog(); // upcasting, oldstyle (C style)
+	Animal* pa = new Dog();
 	pa->makeSound();
+	cout << pa << endl;
 
-	Cat* pc = (Cat*)pa; // Dangerous behavior
+	Cat* pc = (Cat*)pa;  // Dangerous behavior!!!
 	pc->makeSound();
+	cout << pc << endl;
 	delete pc;
 	pc = nullptr;
 
-	// Both results are same.
-	//Dog* pd = (Dog*)pa; // downcasting, oldstyle (C style)
-	//Dog* pd = dynamic_cast<Dog*>(pa);//downcasting, modern style (C++11)
+	//Dog* pd = (Dog*)pa;  // downcasting, old style (C style)
+	//Dog* pd = dynamic_cast<Dog*>(pa);  // downcasting, modern style (C++11)
 	//pd->makeSound();
 	//delete pd;
 	//pd = nullptr;
 
 	return 0;
 }
+// git reset --hard HEAD~»èÁ¦ÇÒÄ¿¹Ô°³¼ö
