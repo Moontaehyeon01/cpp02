c29da4668f4f66ab6b0dc266412cb6a6d86b19b0
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-10-02 11:30:31 +0900

    week06 v1.7 operator overloading

--- week06/app01.cpp
+++ week06/app01.cpp
@@ -30,20 +30,40 @@ public:
 		int r = this->real + right.real;
 		int i = this->imaginary + right.imaginary;
 		return Complex(r, i);
+		//return Complex(r, this->imaginary); // 가능하지만 수학적으로 문제가됨
 	}
-
+	Complex operator++(int) { // 후위연산
+		Complex previous(this->real, this->imaginary);
+		this->real = this->real + 1;
+		return previous;
+	}
+	Complex operator++() { // 전위연산
+		this->real++;
+		return Complex(this->real, this->imaginary);
+	}
 };
 
 int main() {
 	Complex c1;
 	Complex c2(10, 7);
-	
 	c1.setReal(5);
 	c1.setImaginary(3);
-	cout << c1.getReal() << "+" << c1.getImaginary() << "i" << endl;
-	cout << c2.getReal() << "+" << c2.getImaginary() << "i" << endl;
+	/*cout << c1.getReal() << "+" << c1.getImaginary() << "i" << endl;
+	cout << c2.getReal() << "+" << c2.getImaginary() << "i" << endl;*/
 	
-	Complex c3 = c1 + c2;
+	Complex c3 = c1 + c2; // Complex c3 = c1.operator+(c2);
+	//cout << c3.getReal() << "+" << c3.getImaginary() << "i" << endl;
+
+	/*Complex c4 = c3++;*/ // 후위연산
+	Complex c4 = ++c3; // 전위연산
 	cout << c3.getReal() << "+" << c3.getImaginary() << "i" << endl;
+	cout << c4.getReal() << "+" << c4.getImaginary() << "i" << endl;
+
+	//cout << c3++; // real 값만 1증가
+
+	//int i1 = 15;
+	//int i2 = i1++;
+	//cout << i2 << endl;
+
 	return 0;
 }
