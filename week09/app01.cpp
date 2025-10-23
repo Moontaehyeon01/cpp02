cb69a23a74aa9f270564ccacf3ee90d5c08c1bc8
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-10-23 11:21:37 +0900

    week09 v1.9 operator overloading

--- week09/app01.cpp
+++ week09/app01.cpp
@@ -5,48 +5,54 @@ class Complex {
 private:
 	int real;
 	int imaginary;
-
 public:
 	Complex() : real(0), imaginary(0) {
 	}
 	Complex(int real, int imaginary) : real(real), imaginary(imaginary) {
 
 	}
-	//void setReal(int real) : real(real){
+
 	void setReal(int real) {
+		//real = real;
 		this->real = real;
-		//real = real; <<- 쓰레기값 출력됨
 	}
 	int getReal() const {
 		return real;
 	}
-
 	void setImaginary(int imaginary) {
 		this->imaginary = imaginary;
 	}
-	int getImaginary() const {
+	int getimaginary() const {
 		return imaginary;
 	}
-	Complex operator+(const Complex& right) {
-		int r = this->real + right.real;
-		int i = this->imaginary + right.imaginary;
-		return Complex(r, i);
-		//return Complex(r, this->imaginary); // 가능하지만 수학적으로 문제가됨
-	}
-	Complex operator++(int) { // 후위연산
+
+	// member function (method)
+	//Complex operator+(const Complex& right) {
+	//	int r = this->real + right.real;
+	//	int i = this->imaginary + right.imaginary;
+	//	return Complex(r, i);
+	//	//return Complex(r, this->imaginary);  // 가능하나 수학적으로 문제가 됨
+	//}
+	Complex operator++(int) {  // 후위연산
 		Complex previous(this->real, this->imaginary);
 		this->real = this->real + 1;
 		return previous;
 	}
-	Complex operator++() { // 전위연산
+	Complex operator++() {  // 전위연산
 		this->real++;
 		return Complex(this->real, this->imaginary);
 	}
 };
 
-ostream& operator<<(ostream& o, Complex right) { // non member
-	o << right.getReal() << "+" << right.getImaginary() << "i";
+// non member function
+ostream& operator<<(ostream& o, const Complex right) {
+	o << right.getReal() << "+" << right.getimaginary() << "i";
 	return o;
+}
+Complex operator+(const Complex& left, const Complex& right) {
+	int r = left.getReal() + right.getReal();
+	int i = left.getimaginary() + right.getimaginary();
+	return Complex(r, i);
 }
 
 int main() {
@@ -55,11 +61,11 @@ int main() {
 	c1.setReal(5);
 	c1.setImaginary(3);
 
-	Complex c3 = c1 + c2; // Complex c3 = c1.operator+(c2);
+	Complex c3 = c1 + c2;  // Complex c3 = c1.operator+(c2);
 
 	Complex c4 = c3++;
-	/*cout << c3.getReal() << "+" << c3.getImaginary() << "i" << endl;
-	cout << c4.getReal() << "+" << c4.getImaginary() << "i" << endl;*/
+	//cout << c3.getReal() << "+" << c3.getimaginary() << "i" << endl;
+	//cout << c4.getReal() << "+" << c4.getimaginary() << "i" << endl;
 	cout << c3 << endl;
 	cout << c4 << endl;
 	return 0;
