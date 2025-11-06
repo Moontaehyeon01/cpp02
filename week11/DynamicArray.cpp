9df3c71dafa762056f221e2bd7a5f40eb955553d
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-11-06 11:11:09 +0900

    week11 v2.6 exception handling (dynamic array)

--- week11/DynamicArray.cpp
+++ week11/DynamicArray.cpp
@@ -18,3 +18,13 @@ DynamicArray::~DynamicArray()
 	cout << "동적 배열 메모리 해제\n";
 	delete[] ptr;
 }
+
+int DynamicArray::getAt(int index)
+{
+	return ptr[index];
+}
+
+void DynamicArray::setAt(int index, int value)
+{
+	ptr[index] = value;
+}
