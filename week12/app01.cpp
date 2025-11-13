1bb93cdcb5ca43f9328b8ae558a8a386fcbd3c7e
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-11-13 11:54:31 +0900

    week12 v3.3 exception handling (dynamic array)

--- week12/app01.cpp
+++ week12/app01.cpp
@@ -10,17 +10,19 @@ int main()
 
         da1.setAt(0, 100);
         da1.setAt(4, -11);
-        //da1.setAt(10, 9);
-        //da1.setAt(-3, 21);
+        //da1.setAt(10, 9);  // !
+        //da1.setAt(-3, 21); // !
 
         cout << da1.getAt(4) << endl;
         cout << da1.getAt(0) << endl;
-        cout << da1.getAt(7) << endl;
+        //cout < endl;
+        cout << da1.getAt(7) << endl;  // !
     }
     catch (const MyException& err) {
         cout << "에러코드 : " << err.getErrorCode() << '\n';
         cout << "에러메세지 : " << err.getErrorMessage() << '\n';
-        cout << "에러주소 : "<< err.getErrorAddress() << "\n";
+        cout << "에러 메모리 번지 주소 : " << err.getErrorAddress() << '\n';
+
     }
     catch (...) {
         cout << "런타임 예외 발생\n";
