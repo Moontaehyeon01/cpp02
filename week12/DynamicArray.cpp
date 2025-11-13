dad2c57cf03ae50b8c2e0bc1ecc79041c3a82b7a
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-11-13 11:22:24 +0900

    week12 v3.1 exception handling (dynamic array)

--- week12/DynamicArray.cpp
+++ week12/DynamicArray.cpp
@@ -7,39 +7,39 @@ DynamicArray::DynamicArray()
 {
 }
 
-DynamicArray::DynamicArray(int size):size(size) // 초기화리스트 -> :size(size)
+DynamicArray::DynamicArray(int size) : size(size)
 {
 	try {
-		//this->size = size; // this 대신 초기화 리스트 사용가능
+		//this->size = size;	
 		ptr = new int[size];
 		cout << "동적배열 생성됨\n";
-		throw "4885\n";
+		//throw "메모리 오류 발생\n";
+		throw MyException(1000, "메모리 오류 발생", this);
 	}
 	catch (...) {
 		delete[] ptr;
-		cout << "힙메모리 해제(생성자 안쪽)\n";
+		cout << "힙메모리 해제 (생성자 안쪽)\n";
 
 		throw;
 	}
+}
+
+int DynamicArray::getAt(int index)
+{
+	if (index >= size || index < 0)
+		throw MyException(4885, "인덱스 범위를 벗어났습니다. 메모리에 접근할 수 없습니다.\n", this);
+	return ptr[index];
+}
+
+void DynamicArray::setAt(int index, int value)
+{
+	if (index >= size || index < 0)
+		throw MyException(4886, "인덱스 범위를 벗어났습니다. 메모리에 할당 할 수 없습니다.\n", this);;
+	ptr[index] = value;
 }
 
 DynamicArray::~DynamicArray()
 {
 	cout << "동적배열 메모리 해제\n";
 	delete[] ptr;
-}
-
-int DynamicArray::getAt(int index)
-{
-	if (index >= size || index < 0)
-		throw MyException(4885, "인덱스 범위를 벗어났습니다\n", this);
-	return ptr[index];
-}
-
-void DynamicArray::setAt(int index, int value)
-{
-	//if (index < size && index >= 0)
-	if (index >= size || index < 0)
-		throw 0;
-	ptr[index] = value;
 }
