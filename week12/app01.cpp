b5d97182f4e177075e9a0baff02a13c42142ca17
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-11-13 11:35:49 +0900

    week12 v3.2 exception handling (dynamic array)

--- week12/app01.cpp
+++ week12/app01.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "DynamicArray.h"
+#include "MyException.h"
 using namespace std;
 
 int main()
@@ -16,12 +17,10 @@ int main()
         cout << da1.getAt(0) << endl;
         cout << da1.getAt(7) << endl;
     }
-    catch(int err){
-        cout << "인덱스 범위를 벗어났습니다\n";
-    }
-    catch (const char* err) {
-        cout << "메모리 오류\n";
-        cout << "에러코드 : "<< err;
+    catch (const MyException& err) {
+        cout << "에러코드 : " << err.getErrorCode() << '\n';
+        cout << "에러메세지 : " << err.getErrorMessage() << '\n';
+        cout << "에러주소 : "<< err.getErrorAddress() << "\n";
     }
     catch (...) {
         cout << "런타임 예외 발생\n";
