b0292c557b88adff1cbd424e46bc5c596be85f57
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-11-20 12:19:44 +0900

    week13 v3.7 function template

--- week13/app01.cpp
+++ week13/app01.cpp
@@ -1,28 +1,24 @@
 #include <iostream>
 using namespace std;
 
-// 템플릿 함수 정의
 template <typename T>
-void swap(T* first, T* second)
+T bigger(T first, T second)
 {
-	T temp = *first;
-	*first = *second;
-	*second = temp;
+	if (first > second) 
+		return first;
+	return second;
 }
 
 int main()
 {
-	// int 자료형 스왑
-	int i1 = 5;
-	int i2 = 70;
-	swap(&i1, &i2);
-	cout << "swap(5, 70) = ";
-	cout << i1 << "  " << i2 << endl;
-	// double 자료형 스왑
-	double d1 = 51.5;
-	double d2 = 42.7;
-	swap(d1, d2);
-	cout << "swap(51.5, 42.7) = ";
-	cout << d1 << "  " << d2 << endl;
+	int i1 = 11;
+	double d1 = 11.97;
+	
+	//cout << "swap(51.5, 42.7) = ";
+	cout << bigger<double>(i1, d1) << endl; // 명시적 호출
+	cout << bigger<double>(55.7, 11.9) << endl;
+	cout << bigger(55.7, 11.9) << endl;
+	cout << bigger<string>("Hello", "Hi") << endl;
+
 	return 0;
 }
