855dcaf6d07289f8c0a2ba8210d1dd56d4a27495
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-11-27 12:20:47 +0900

    week13 v4.1 STL(iterator, vector)

---
+++ week14/app02.cpp
@@ -0,0 +1,43 @@
+//#include <vector>
+//#include <iostream>
+//#include <iomanip>
+//using namespace std;
+//
+//int main()
+//{
+//    //vector<int> vec(10);
+//    vector<int> vec;
+//    vector<int>::iterator iter;
+//    vector<int>::reverse_iterator rIter;
+//    
+//    for (int i = 0; i < 10; i++)
+//    {
+//        //vec.at(i) = i * i;
+//        //vec[i] = i * i;
+//        vec.push_back(i * i);
+//    }
+//    
+//    cout << "±âº» Å½»ö: ";
+//    for (iter = vec.begin(); iter != vec.end(); ++iter)
+//    {
+//        cout << setw(4) << *iter;
+//    }
+//    cout << endl;
+//
+//    vec.pop_back();
+//    
+//    cout << "¿ª Å½»ö: ";
+//    for (rIter = vec.rbegin(); rIter != vec.rend(); ++rIter)
+//    {
+//        cout << setw(4) << *rIter;
+//    }
+//    cout << endl;
+//    iter = vec.begin();
+//    iter = iter + 3;
+//    cout << *iter << "\n";
+//    rIter = vec.rbegin();
+//    rIter = rIter + 6;
+//    cout << *rIter << "\n";
+//
+//    return 0;
+//}
