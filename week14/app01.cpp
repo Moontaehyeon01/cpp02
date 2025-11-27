21b582c8e23c8b357858616f19f1acfd55cab619
Moontaehyeon01 <202030413@daelim.ac.kr>
2025-11-27 11:42:01 +0900

    week13 v4.0 class template

--- week14/app01.cpp
+++ week14/app01.cpp
@@ -6,11 +6,16 @@ private:
 	string name;
 public:
 	Pokemon() : hp(1), name("무명") {
-	
-	};
 
+	}
 	Pokemon(int hp, string name) : hp(hp), name(name) {
-		cout << name << "포켓몬 생성됨\n";
+		cout << name << " 포켓몬 생성됨\n";
+	}
+	int getHp() const {
+		return hp;
+	}
+	string getName() const {
+		return name;
 	}
 };
 //class Pikachu : public Pokemon {
@@ -24,24 +29,37 @@ public:
 //	void attack() { "물대포공격!\n"; };
 //};
 
+ostream& operator<<(ostream& o, Pokemon& right) {
+	o << right.getName() << "(hp: " << right.getHp() << ")\n";
+	return o;
+}
+
 int main()
 {
+	typedef Stack<Pokemon> PokemonStack;
+	typedef Stack<int> iStack;
+
 	Pokemon pikachu;
 	Pokemon squirtle(100, "꼬부기");
 
-  Stack<int> stacki(10);
-  Stack<Pokemon> stackPokemon(3);
-  Stack<double> stackd(3);
-  stackPokemon.push(pikachu);
-  stackPokemon.push(squirtle);
+	iStack stacki(10);
+	PokemonStack stackp(3);
+	Stack<double> stackd(3);
+	stackp.push(pikachu);
+	stackp.push(squirtle);
 
-  stacki.push(5);
-  stacki.push(6);
-  stacki.push(7);
-  stacki.push(3);
-  cout << stacki.pop() << endl;
-  cout << stacki.pop() << endl;
-  cout << stackd.pop() << endl;
+	stacki.push(5);
+	stacki.push(6);
+	stacki.push(7);
+	stacki.push(3);
+	cout << stacki.pop() << endl;
+	cout << stacki.pop() << endl;
+	//cout << stackd.pop() << endl;  // stack is empty!
+	stackp.pop();
+	stackp.pop();
+	//stackp.pop();
 
-  return 0;
+	cout << pikachu;
+	cout << squirtle;
+	return 0;
 }
